ab259558d2955e93c381daf749f3b22e379734db
Mostafa-Farghly <mostafa.alaa.16897@gmail.com>
2021-06-21 19:18:12 +0200

    Implement database URL

--- projects/01_fyyur/starter_code/config.py
+++ projects/01_fyyur/starter_code/config.py
@@ -9,5 +9,5 @@ DEBUG = True
 # Connect to the database
 
 
-# TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = '<Put your local database url>'
+# DATABASE URL
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:aezakmi97@localhost:5432/fyyurapp'
