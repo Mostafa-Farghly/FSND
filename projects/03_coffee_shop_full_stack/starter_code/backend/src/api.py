323011dcc99f6877092016bc1dea4e9cfdb58a76
Mostafa-Farghly <mostafa.alaa.16897@gmail.com>
2021-08-14 15:53:29 +0200

    Implement get drinks endpoint

--- projects/03_coffee_shop_full_stack/starter_code/backend/src/api.py
+++ projects/03_coffee_shop_full_stack/starter_code/backend/src/api.py
@@ -12,22 +12,32 @@ setup_db(app)
 CORS(app)
 
 '''
-@TODO uncomment the following line to initialize the datbase
+uncomment the following line to initialize the datbase
 !! NOTE THIS WILL DROP ALL RECORDS AND START YOUR DB FROM SCRATCH
 !! NOTE THIS MUST BE UNCOMMENTED ON FIRST RUN
 !! Running this funciton will add one
 '''
-# db_drop_and_create_all()
+db_drop_and_create_all()
 
 # ROUTES
 '''
-@TODO implement endpoint
+implement endpoint
     GET /drinks
         it should be a public endpoint
         it should contain only the drink.short() data representation
     returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
         or appropriate status code indicating reason for failure
 '''
+@app.route("/drinks")
+def get_drinks():
+    drinks = Drink.query.all()
+
+    drinks = [drink.short() for drink in drinks]
+
+    return jsonify({
+        'success': True,
+        "drinks": drinks
+    })
 
 
 '''
